8df7526a317eebe874062779f831ff8935deae86
Emir <pooky03@gmail.com>
2020-11-21 16:55:56 -0600

    feat() handle error and loading state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import { useEffect, useState } from 'react';
 import { URL_API } from './helper'
+import { Loader } from 'semantic-ui-react';
 
 function App() {
   const [searchValue, setSearchValue] = useState('books');
   const [images, setImages] = useState([]);
+  const [isLoading, setLoading] = useState(false)
 
-  const fetchData = async() => {
-    let flickrData = await fetch(URL_API + searchValue);
-    let flickrJSON = await flickrData.json();
-    setImages(flickrJSON.photos.photo);
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      let flickrData = await fetch(URL_API + searchValue);
+      let flickrJSON = await flickrData.json();
+      setImages(flickrJSON.photos.photo);
+    } catch (error) {
+      alert(error);
+    }
+
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -21,6 +31,7 @@ function App() {
 
   return (
     <>
+      <Loader content='Loading' active={isLoading} size='massive'/>
       <SearchInput onChange={setSearchValue} onClick={fetchData}/>
       <Grid images={images}/>
     </>
