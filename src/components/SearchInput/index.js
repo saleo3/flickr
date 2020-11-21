e8112307a98a9e5cb601cb3bbb5b3d84b94d9282
Emir <pooky03@gmail.com>
2020-11-21 15:02:36 -0600

    feat() Added SearchInput component

---
+++ src/components/SearchInput/index.js
@@ -0,0 +1,13 @@
+import { Input } from 'semantic-ui-react';
+import './styles.css'
+
+function SearchInput({ onChange, onClick }) {
+  return <Input
+    className="search_button"
+    action={{icon: 'search', onClick: event => onClick()}}
+    placeholder='Search...'
+    onChange={event => onChange(event.target.value)}
+  />
+}
+
+export default SearchInput;
